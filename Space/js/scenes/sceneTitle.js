1924b84ba14077e545e150f92e4d1f004b17fa67
Zachary Fiel <robloxzacfiel@gmail.com>
2019-05-25 12:39:38 +0800

    final fix

--- Space/js/scenes/sceneTitle.js
+++ Space/js/scenes/sceneTitle.js
@@ -14,11 +14,11 @@ class SceneTitle extends Phaser.Scene {
 
         this.add.image(0, 0, 'background').setOrigin(0.5, 0,5)
         this.alignGrid = new AlignGrid({rows: 11, cols:11, scene:this})
-        this.alignGrid.showNumbers()
+        //this.alignGrid.showNumbers()
 
         var title = this.add.image(0, 0, "title")
         Align.scaleToGameW(title, .8)
-        //  this.alignGrid.placeAtIndex(27, title)
+        this.alignGrid.placeAtIndex(27, title)
 
         var ship = this.add.image(0, 0, 'ship')
         this.alignGrid.placeAtIndex(60, ship)
