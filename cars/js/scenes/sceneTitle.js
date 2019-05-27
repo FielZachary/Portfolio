5418d5dd9dd856b06f1d294e68a3c3f853ac128a
Zachary Fiel <robloxzacfiel@gmail.com>
2019-05-27 17:13:56 +0800

    fix 31

--- cars/js/scenes/sceneTitle.js
+++ cars/js/scenes/sceneTitle.js
@@ -30,11 +30,11 @@ class SceneTitle extends Phaser.Scene {
 
         this.text1 = this.add.text(12, 0, 'Instructions:')
         this.text1.y = 541
-        this.text2 = this.add.text(0, 0, 'The objective is to match the ball color')
-        this.text3 = this.add.text(this.text1.x, 575, 'to the paddle color and survive as long as possible.')
+        this.text2 = this.add.text(0, 0, 'The objective is to survive for as long')
+        this.text3 = this.add.text(this.text1.x, 575, 'as possible while dodging the obstacles.')
         this.text4 = this.add.text(this.text1.x, 592, 'Controls:')
-        this.text5 = this.add.text(this.text1.x, 609, 'Click to change the paddle color')
-        this.text6 = this.add.text(this.text1.x, 626, 'Test')
+        this.text5 = this.add.text(this.text1.x, 609, 'Click to switch sides')
+        //this.text6 = this.add.text(this.text1.x, 626, 'Test')
         //this.alignGrid.placeAtIndex(110, this.text2)
         this.text2.y = 560
         this.text2.x = this.text1.x
