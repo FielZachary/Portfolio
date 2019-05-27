ef08691e8c52b37327045e7dddf7c19595a2cafd
Zachary Fiel <robloxzacfiel@gmail.com>
2019-05-27 21:21:43 +0800

    fix 37

--- invaders/js/scenes/sceneTitle.js
+++ invaders/js/scenes/sceneTitle.js
@@ -20,11 +20,22 @@ class SceneTitle extends Phaser.Scene {
         Align.scaleToGameW(title, .8)
         Align.scaleToGameW(title2, .8)
         this.alignGrid.placeAtIndex(38, title)
-        this.alignGrid.placeAtIndex(49, title2)
+        this.alignGrid.placeAtIndex(60, title2)
 
 
         var btnStart = new FlatButton({scene:this, key:"button1", text:"Start!", x:240, y:100, event:"start_game"}) 
         this.alignGrid.placeAtIndex(93, btnStart)
+
+        this.text1 = this.add.text(12, 0, 'Instructions:')
+        this.text1.y = 541
+        this.text2 = this.add.text(0, 0, 'The objective is shoot all invaders')
+        this.text3 = this.add.text(this.text1.x, 575, 'before the reach to the bottom.')
+        this.text4 = this.add.text(this.text1.x, 592, 'Controls:')
+        this.text5 = this.add.text(this.text1.x, 609, 'A - To go Right, D - To go left')
+        this.text6 = this.add.text(this.text1.x, 626, 'Space - To shoot')
+        //this.alignGrid.placeAtIndex(110, this.text2)
+        this.text2.y = 560
+        this.text2.x = this.text1.x
 
         
 
